06d631c62bb973c7d733aabd23d417eed85d20fc
Andrew Shaffer <darawk@gmail.com>
2015-08-24 10:06:11 -0700

    dont use es6 for now

--- test/index.js
+++ test/index.js
@@ -9,6 +9,8 @@ import cookie from '..'
  * Tests
  */
 
-test('should work', () => {
-
+test('should work', ({deepEqual, end}) => {
+  deepEqual(cookie('test').toJSON(), {type: 'EFFECT', payload: {type: 'GET_COOKIE', name: 'test'}, meta: {then: []}})
+  deepEqual(cookie('test', 'testing').toJSON(), {type: 'EFFECT', payload: {type: 'SET_COOKIE', name: 'test', value: 'testing'}, meta: {then: []}})
+  end()
 })
