467cf4b6df1a0b44f501d63f1d76dc63c5dcf20f
Andrew Shaffer <andrew@weo.io>
2015-09-06 15:22:36 -0700

    add ability to specify cookie metadata

--- src/index.js
+++ src/index.js
@@ -8,10 +8,11 @@ import createEffect from 'declarative-effect'
  * Cookie action creator
  */
 
-function cookie (name, value) {
+function cookie (name, value, opts) {
   const effect = {name}
 
-  if (arguments.length === 2) {
+  if (arguments.length > 1) {
+    if (opts) effect.meta = opts
     effect.type = 'SET_COOKIE'
     effect.value = value
   } else {
